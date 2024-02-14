ddf0d44c426ebf1dfc2d929d69c3c2fad2cf4f3b
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-02-14 20:46:58 -0300

    Add Resume and Form components to App.js

--- Front/profinance/src/App.js
+++ Front/profinance/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import GlobalStyle from './styles/global.js'  
 import  Header from "./components/Header/index.js"; 
+import Resume from "./components/Resume/index.js";
+import Form from "./components/Form/index.js";
+
 
 const App = () => { 
     return (
         <div>
-            <Header />  
+              <Header />  
+             <Resume />
+             <Form  />
             <GlobalStyle />  
         </div>
     )   
