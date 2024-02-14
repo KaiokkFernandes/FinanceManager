252e1f25f84249abbb8b1069c58419a966e7bebf
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-02-14 19:54:11 -0300

    Delete unnecessary files and update App component

---
+++ Front/profinance/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./style";
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>Gerenciamento de controle</C.Title>
+      </C.Header>
+    </C.Container>
+  );
+};
+
+export default Header;
